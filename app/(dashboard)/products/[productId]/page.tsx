937a80f034ed5ab4b34662c9825bb9e9e0887578
VACHAN SHETTY <vachan316@gmail.com>
2024-05-07 15:00:54 +0530

    final?

---
+++ app/(dashboard)/products/[productId]/page.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import Loader from "@/components/custom ui/Loader";
+import ProductForm from "@/components/products/ProductForm";
+import React, { useEffect, useState } from "react";
+
+const ProductDetails = ({ params }: { params: { productId: string } }) => {
+  const [loading, setLoading] = useState(true);
+  const [productDetails, setProductDetails] = useState<ProductType | null>(
+    null
+  );
+
+  useEffect(() => {
+    const getProductDetails = async () => {
+      try {
+        const res = await fetch(`/api/products/${params.productId}`, {
+          method: "GET",
+        });
+        const data = await res.json();
+        setProductDetails(data);
+        setLoading(false);
+      } catch (err) {
+        console.log("[productId_GET]", err);
+      }
+    };
+    getProductDetails();
+  }, [params.productId]);
+
+  return loading ? <Loader /> : <ProductForm initialData={productDetails} />;
+};
+
+export default ProductDetails;
